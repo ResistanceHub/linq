7d175d54e3764d22dddf8f72839e2106a1cd22e4
Daryn <daryn.holmes@insightsoftware.com>
2018-06-28 15:40:15 +0100

    initial commit

---
+++ LINQ/Filter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+	public class Filter
+	{
+		public bool contains(int num, int[] list)
+		{
+			return list.Contains(num);
+//			bool result = list.Contains(num);
+//			return result;
+
+//			if (result)
+//			{
+//				return result;
+//			}
+//			else
+//			{
+//				return result;
+//			}
+			
+			
+		}
+
+		private bool IsEven(int n)
+		{
+			// if it is even, then mod 2 is 0
+			// if number modules 2 is 0, then the number is even
+			return (n % 2 == 0);
+		}
+
+		public int[] filterOutOddNumbers(int[] inputList)
+		{
+			IEnumerable<int> result = inputList.Where((num) => IsEven(num));
+//			return result.ToArray();
+
+
+
+			Console.WriteLine("Please enter new content for the file - type exit and press enter to finish editing:");
+			using (StreamWriter sw = new StreamWriter("test.txt")) {
+
+				string newContent = Console.ReadLine();
+
+				while (newContent != "exit") {
+					sw.Write(newContent + Environment.NewLine);
+					Console.Write("Enter a word (or exit to quite):");
+					newContent = Console.ReadLine();
+				}
+			}
+		}
+
+
+	}
+}
---
+++ LINQ/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			int[] numbs = new int[3] {3, 6, 2};
+			int num1 = 6;
+			Filter filterInstance = new Filter();
+
+			bool result = filterInstance.contains(num1, numbs);
+			int[] evens = filterInstance.filterOutOddNumbers(numbs);
+
+			var x = "hello" + Environment.NewLine;
+
+
+			String[] names = new String[3] { "Bob", "Sue", "Daryn" };
+			var shortNames = names.Where((n) => n.Length <= 3).ToArray();
+
+
+			int[] numbers = new int[7] {1000, 1, 2, 3, 4, 5, 6};
+			IEnumerable<int> doubled = numbers.Select((int num) => num * 2);
+			var largenumbers = doubled.Where((int num) => num > 5);
+			int sumOfLargeNumbers = largenumbers.Sum();
+
+			IEnumerable<int> listA = numbers.Select((int num) => num * 2);
+
+			List<int> list = new List<int>();
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				int dbl = numbers[i] * 2;
+				list.Add(dbl);
+			}
+
+
+			foreach (int num in doubled)
+			{
+				Console.Write(num + " ");
+			}
+			Console.WriteLine();
+			foreach (int num in largenumbers) 
+			{
+//				Console.Write(num + " ");
+			}
+
+
+			Console.ReadKey();
+		}
+	}
+}
